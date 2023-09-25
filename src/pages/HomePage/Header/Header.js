b2da6f181d96711e8ec873024bdd05935090e07c
Nikita70770 <filippovni1999@gmail.com>
2023-09-25 17:48:49 +0300

    Move the contents of the Home component to a separate component called Header

---
+++ src/pages/HomePage/Header/Header.js
@@ -0,0 +1,30 @@
+/* import css styles */
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header-wrapper">
+                <div className="header-text">
+                    <p>Welcome!</p>
+                </div>
+                <h1 className="header-title">
+                    <strong>
+                        My name is <em>Nikita</em>
+                    </strong>
+                    <br />a frontend developer
+                </h1>
+                <div className="header-subtitle">
+                    <p>with passion for learning and creating.</p>
+                </div>
+                <div className="header-button">
+                    <a className="btn-download-cv" href="#!">
+                        Download CV
+                    </a>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,32 +1,10 @@
 /* import Components */
+import Header from './Header/Header';
 
 /* import css styles */
-import './Home.css';
 
 const Home = () => {
-    return (
-        <header className="header">
-            <div className="header-wrapper">
-                <div className="header-text">
-                    <p>Welcome!</p>
-                </div>
-                <h1 className="header-title">
-                    <strong>
-                        My name is <em>Nikita</em>
-                    </strong>
-                    <br />a frontend developer
-                </h1>
-                <div className="header-subtitle">
-                    <p>with passion for learning and creating.</p>
-                </div>
-                <div className="header-button">
-                    <a className="btn-download-cv" href="#!">
-                        Download CV
-                    </a>
-                </div>
-            </div>
-        </header>
-    );
+    return <Header />;
 };
 
 export default Home;
